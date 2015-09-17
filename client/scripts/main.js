f32fd9d2bc5e29c44706ffeb7beb97e50709605c
ddn_shep <andyclass50@hotmail.com>
2015-09-17 12:11:55 +0000

    Second commit from C9: improvements to blog design

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -4,13 +4,39 @@
   var $window = $('.plx'),
       $nav = $('.navbar'),
       $header = $('header'),
+      $top = $('#back-to-top'),
       toggle = function() {
-        if ($window.scrollTop() > 300) $nav.addClass('small');
-        else $nav.removeClass('small');
+        var top = $window.scrollTop(),
+            small = 'small';
         
-        if ($window.scrollTop() > 50) $header.addClass('small');
-        else $header.removeClass('small');
+        if (top > 50) {
+          $header.addClass(small);
+          $top.fadeIn();
+
+          if (top > 300) $nav.addClass(small);
+          else $nav.removeClass(small);
+        }
+        else {
+          $header.removeClass(small);
+          $top.fadeOut();
+        }
+      },
+      top = function() {
+        $('.plx').animate({ scrollTop: 0 }, 600);
       };
-  
+
+  $top.off('click', top).on('click', top);
   $window.off('scroll', toggle).on('scroll', toggle);
+})();
+
+;(function() {
+  'use strict';
+
+  var $nav = $('#top-nav'),
+      $toggle = $('#top-nav-toggle'),
+      toggle = function() {
+        $nav.toggleClass('hidden-md-down');
+      };
+
+  $toggle.off('click', toggle).on('click', toggle);
 })();
